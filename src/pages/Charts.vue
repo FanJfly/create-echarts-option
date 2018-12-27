769068058b38510841d5394e5901e205d05c3318
jiangf <jiangf@chinawyny.com>
2018-12-27 16:05:31 +0800

    [feat] 添加单条曲线的颜色的配置操作；添加format option

--- src/pages/Charts.vue
+++ src/pages/Charts.vue
@@ -22,18 +22,18 @@
         </el-form-item>
 
         <el-form-item label="是否显示x轴刻度 ">
-          <el-select v-model="hasXSplit">   
-            <el-option value="1">是</el-option>
-            <el-option value="2">否</el-option>
+          <el-select v-model="hasXSplit" @change="(value) => setAxisConfig(value, 'x', 'tick')">   
+            <el-option value="是">是</el-option>
+            <el-option value="否">否</el-option>
           </el-select>
         </el-form-item>
 
         <el-form-item label="x轴label颜色">
-          <el-color-picker v-model="xLabelColor"></el-color-picker>
+          <el-color-picker v-model="xLabelColor" @change="(value) => setAxisConfig(value, 'x', 'labelColor')"></el-color-picker>
         </el-form-item>
 
-        <el-form-item label="x轴label颜色">
-          <el-color-picker v-model="xSplitColor"></el-color-picker>
+        <el-form-item label="x轴颜色">
+          <el-color-picker v-model="xSplitColor" @change="(value) => setAxisConfig(value, 'x', 'axisColor')"></el-color-picker>
         </el-form-item>
 
         <el-form-item label="y轴数据">
@@ -41,30 +41,49 @@
         </el-form-item>
 
         <el-form-item label="是否显示y轴刻度 ">
-          <el-select v-model="hasYSplit">   
-            <el-option value="1">是</el-option>
-            <el-option value="2">否</el-option>
+          <el-select v-model="hasYSplit" @change="(value) => setAxisConfig(value, 'y', 'tick')">   
+            <el-option value="是">是</el-option>
+            <el-option value="否">否</el-option>
           </el-select>
         </el-form-item>
 
         <el-form-item label="y轴label颜色">
-          <el-color-picker v-model="yLabelColor"></el-color-picker>
+          <el-color-picker v-model="yLabelColor"  @change="(value) => setAxisConfig(value, 'y', 'labelColor')"></el-color-picker>
         </el-form-item>
 
-        <el-form-item label="y轴label颜色">
-          <el-color-picker v-model="ySplitColor"></el-color-picker>
+        <el-form-item label="y轴颜色">
+          <el-color-picker v-model="ySplitColor"  @change="(value) => setAxisConfig(value, 'y', 'axisColor')"></el-color-picker>
         </el-form-item>
 
         <el-form-item label="线条是否渐变">
           <el-select v-model="isGrent">
-            <el-option value="1">是</el-option>
-            <el-option value="2">否</el-option>
+            <el-option value="是">是</el-option>
+            <el-option value="否">否</el-option>
           </el-select>
         </el-form-item>
 
-        <el-form-item label="渐变始色-渐变结色">
+        <el-form-item label="线条渐变始色-渐变结色">
           <el-color-picker v-model="grentStartColor"></el-color-picker>
-          <el-color-picker v-model="grentEndColor"></el-color-picker>
+          <el-color-picker v-model="grentEndColor"  @change="(value) => setSeriesConfig('lineGrent')"></el-color-picker>
+        </el-form-item>
+
+        <el-form-item label="是否平滑">
+          <el-select v-model="isSmooth" @change="(value) => setSeriesConfig('toSmooth',value)">
+            <el-option value="是">是</el-option>
+            <el-option value="否">否</el-option>
+          </el-select>
+        </el-form-item>
+
+        <el-form-item label="是否显示为面积图">
+          <el-select v-model="isArea" @change="() => setSeriesConfig('toArea', value)">
+            <el-option value="是">是</el-option>
+            <el-option value="否">否</el-option>
+          </el-select>
+        </el-form-item>
+
+        <el-form-item label="面积图渐变始色-渐变结色">
+          <el-color-picker v-model="areaGrentStartColor"></el-color-picker>
+          <el-color-picker v-model="areaGrentEndColor"  @change="(value) => setSeriesConfig('areaGrent')"></el-color-picker>
         </el-form-item>
 
       </el-form>
@@ -72,12 +91,17 @@
       
       
     </el-aside>
-    <el-aside>
-      <codemirror v-model="code" :options="cmOptions" @change="change"></codemirror>
-    </el-aside>
+  <!--   <el-aside>
+      <codemirror :value="option" :options="cmOptions" @change="change"></codemirror>
+    </el-aside> -->
     <el-main>
-      <chart :options="option"></chart>
-      <div v-text="option"></div>
+      <ECharts :options="option"></ECharts>
+      <json-viewer
+        :value="option"
+        :expand-depth=5
+        copyable
+        boxed
+      />
     </el-main>
   </el-container>
 </template>
@@ -86,59 +110,67 @@
 import "echarts/lib/chart/bar";
 // built-in theme
 import "echarts/theme/dark";
-import { codemirror } from 'vue-codemirror'
-import 'codemirror/lib/codemirror.css'
-import { stringify } from 'qs'
-// custom theme
-// import theme from './theme.json'
+import ECharts from "../components/ECharts";
+import echarts from "echarts";
+import JsonViewer from "vue-json-viewer";
+
 export default {
   name: "charts",
   data() {
     let data = [];
 
     for (let i = 0; i <= 360; i++) {
-      let t = i / 180 * Math.PI;
+      let t = (i / 180) * Math.PI;
       let r = Math.sin(2 * t) * Math.cos(2 * t);
       data.push([r, i]);
     }
     return {
       msg: "Welcome to Your Vue.js App",
       form: {},
+      formatOption: "",
       cmOptions: {
-        mode: this.mode,
         tabSize: 2,
         lineNumbers: true,
         lineWrapping: true,
-        extraKeys: {'Ctrl-Space': 'autocomplete'},
+        extraKeys: { "Ctrl-Space": "autocomplete" },
+        styleActiveLine: true,
+        line: true,
+        mode: "text/javascript",
+        theme: "default"
       },
       option: {
         title: {
-          text: ''
+          text: ""
         },
         xAxis: {
-          type: 'category',
-          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+          type: "category",
+          data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
         },
         yAxis: {
-          type: 'value'
+          type: "value"
         },
-        series: [{
-          data: [820, 932, 901, 934, 1290, 1330, 1320],
-          type: 'line'
-        }]
+        series: [
+          {
+            data: [820, 932, 901, 934, 1290, 1330, 1320],
+            type: "line"
+          }
+        ]
       },
-      code: "option = {a: 1,b:'c',c:['a','b','c']}",
-      xData: '',
-      yData: '',
-      hasXSplit: 1,
-      xLabelColor: '#000',
-      xSplitColor: '#000',
-      hasYSplit: 1,
-      yLabelColor: '#000',
-      ySplitColor: '#000',
-      isGrent: 1,
-      grentStartColor: '#000',
-      grentEndColor: '#fff',
+      xData: "",
+      yData: "",
+      hasXSplit: "是",
+      xLabelColor: "#000",
+      xSplitColor: "#000",
+      hasYSplit: "是",
+      yLabelColor: "#000",
+      ySplitColor: "#000",
+      isGrent: "否",
+      grentStartColor: "#000",
+      grentEndColor: "#fff",
+      areaGrentStartColor: "#000",
+      areaGrentEndColor: "#fff",
+      isSmooth: "否",
+      isArea: "否",
       type: [
         {
           value: "line",
@@ -151,18 +183,135 @@ export default {
         {
           value: "pie",
           label: "饼图"
-        },
+        }
       ],
       value: ""
     };
   },
   methods: {
-    change: function(code) {
-      console.log(code)
+    getConfig(attr, value) {
+      let setting = {};
+      switch (attr) {
+        //是否显示坐标轴刻度
+        case "tick":
+          setting = {
+            axisTick: {
+              show: value === "是" ? true : false
+            }
+          };
+          return setting;
+          break;
+        //设置坐标轴颜色刻度
+        case "labelColor":
+          setting = {
+            axisLabel: {
+              textStyle: {
+                color: value
+              }
+            }
+          };
+          return setting;
+          break;
+        //设置坐标轴颜色
+        case "axisColor":
+          setting = {
+            axisLine: {
+              lineStyle: {
+                color: value
+              }
+            }
+          };
+          return setting;
+          break;
+        //设置折线图线条是否平滑
+        case "toSmooth":
+          setting = {
+            smooth: value === "是" ? true : false
+          };
+          return setting;
+          break;
+        //设置是否为面积图
+        case "toArea":
+          value === "是"
+            ? (setting = {
+                areaStyle: {}
+              })
+            : {};
+          return setting;
+          break;
+        //设置线条渐变
+        case "lineGrent":
+          setting = {
+            itemStyle: {
+              color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                {
+                  offset: 1,
+                  color: this.grentStartColor
+                },
+                {
+                  offset: 0,
+                  color: this.grentEndColor
+                }
+              ])
+            }
+          };
+          return setting;
+          break;
+        //设置面积图渐变
+        case "areaGrent":
+          setting = {
+            areaStyle: {
+              color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                {
+                  offset: 0,
+                  color: this.areaGrentStartColor
+                },
+                {
+                  offset: 1,
+                  color: this.areaGrentEndColor
+                }
+              ])
+            }
+          };
+          return setting;
+          break;
+        default:
+          return setting;
+          break;
+      }
+    },
+
+    setAxisConfig(value, axis, attr) {
+      let setting = this.getConfig(attr, value);
+      this.option[`${axis}Axis`] = {
+        ...this.option[`${axis}Axis`],
+        ...setting
+      };
+    },
+
+    setSeriesConfig(attr, value) {
+      let setting = this.getConfig(attr, value);
+      this.option["series"] = this.option["series"].map(item => {
+        item = {
+          ...item,
+          ...setting
+        };
+        return item;
+      });
+    },
+  },
+  watch: {
+    option: {
+      handler(curVal, oldVal) {
+        console.log(curVal.toString(), oldVal.toString());
+        this.formatOption = curVal;
+      },
+      deep: true
     }
   },
   components: {
-    codemirror
+    ECharts,
+    JsonViewer
   }
 };
 </script>
